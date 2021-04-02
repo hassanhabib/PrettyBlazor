0aa0404965e6b8810a930327fa8fea87c8c635f5
Hassan Habib <hassanhabib@live.com>
2021-04-02 01:49:28 -0700

    Add project files.

---
+++ PrettyBlazor/Condition.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PrettyBlazor
+{
+    public partial class Condition : ComponentBase
+    {
+        [Parameter]
+        public bool Evaluation { get; set; }
+
+        [Parameter]
+        public RenderFragment Match { get; set; }
+
+        [Parameter]
+        public RenderFragment NotMatch { get; set; }
+    }
+}
