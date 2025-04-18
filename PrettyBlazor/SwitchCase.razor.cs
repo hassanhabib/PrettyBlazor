d846419c098d98fc61d5c263a878a2401c9807f5
Mabrouk Mahdhi <coding@mahdhi.com>
2025-04-18 02:11:47 +0200

    COMPONENTS: Define Switch Case Views

---
+++ PrettyBlazor/SwitchCase.razor.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace PrettyBlazor
+{
+    public partial class SwitchCase<T> : ComponentBase
+    {
+        [CascadingParameter(Name = "SwitchValue")]
+        public T SwitchValue { get; set; }
+
+        [Parameter] public T When { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        private bool ShouldRenderCase() =>
+            EqualityComparer<T>.Default.Equals(SwitchValue, When);
+    }
+}
---
+++ PrettyBlazor/SwitchDefault.razor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace PrettyBlazor
+{
+    public partial class SwitchDefault<T> : ComponentBase
+    {
+        [CascadingParameter(Name = "SwitchValue")]
+        public T SwitchValue { get; set; }
+
+        [CascadingParameter] public List<T> CaseValues { get; set; }
+
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        private bool ShouldRenderDefaultCase() =>
+            CaseValues == null || !CaseValues.Contains(SwitchValue);
+    }
+}
