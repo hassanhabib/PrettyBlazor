69906f049172dc979657e50b501886bb58ad45e7
Mabrouk Mahdhi <coding@mahdhi.com>
2025-04-18 02:16:24 +0200

    ShouldHaveDefaultInitParametersForSwitch -> PASS

---
+++ PrettyBlazor.Tests/Switchs/SwitchTests.Render.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using FluentAssertions;
+using Xunit;
+
+namespace PrettyBlazor.Tests.Switchs
+{
+    public partial class SwitchTests
+    {
+        [Fact]
+        public void ShouldHaveDefaultInitParametersForSwitch()
+        {
+            // when
+            var component = new Switch<string>();
+
+            // then
+            component.Value.Should().BeNull();
+            component.ChildContent.Should().BeNull();
+        }
+    }
+}
---
+++ PrettyBlazor.Tests/Switchs/SwitchTests.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Bunit;
+
+namespace PrettyBlazor.Tests.Switchs
+{
+    public partial class SwitchTests : TestContext
+    {
+        private IRenderedComponent<Switch<int>> renderedSwitchComponent;
+    }
+}
